f91e2cae835d3b68b18c762e90e48fb265410e90
Matt <matthewgstillman@gmail.com>
2018-08-10 13:50:52 -0700

    First Commit

---
+++ apps/pro_publica_spending_api/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ProPublicaSpendingApiConfig(AppConfig):
+    name = 'pro_publica_spending_api'
---
+++ apps/pro_publica_spending_api/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+import requests
+
+# Create your views here.
+def index(request):
+    url = ("https://projects.propublica.org/free-the-files/markets.json")
+    response = requests.get(url)
+    print("Response: " + str(response))
+    spending = response.json()
+    print("Spending: " + str(spending))
+    context = {
+        'response': response,
+        'spending': spending,
+    }
+    return render(request, 'pro_publica_spending_api/index.html', context)
+
+def comms(request):
+    url = ("https://projects.propublica.org/free-the-files/committees.json")
+    response = requests.get(url)
+    print("Response: " + str(response))
+    comms = response.json()
+    print("Committees: " + str(comms))
+    context = {
+        'response': response,
+        'comms': comms,
+    }
+    return render(request, 'pro_publica_spending_api/comms.html', context)
