fdeffa516b9a54d153077653220b9d70c35bedda
Matt <matthewgstillman@gmail.com>
2018-08-10 19:05:13 -0700

    Additional Filing Functionality

--- apps/pro_publica_spending_api/urls.py
+++ apps/pro_publica_spending_api/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^comms$', views.comms, name="comms"),
+    url(r'^filing$', views.filing, name="filing"),
 ]
--- apps/pro_publica_spending_api/views.py
+++ apps/pro_publica_spending_api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 import requests
+import json
+import jsondate
 
 # Create your views here.
 def index(request):
@@ -15,6 +17,21 @@ def index(request):
         'sorted_spending': sorted_spending,
     }
     return render(request, 'pro_publica_spending_api/index.html', context)
+
+def filing(request):
+    url = ("https://projects.propublica.org/free-the-files/filings/15399.json")
+    response = requests.get(url)
+    print("Response: " + str(response))
+    filings = response.json()
+    print("Filings: " + str(filings))
+    upload_date = filings['filing']
+    ['upload_date']
+    print(upload_date)
+    context = {
+        'response': response,
+        'filings': filings,
+    }
+    return render(request, 'pro_publica_spending_api/filing.html', context)
 
 def comms(request):
     url = ("https://projects.propublica.org/free-the-files/committees.json")
